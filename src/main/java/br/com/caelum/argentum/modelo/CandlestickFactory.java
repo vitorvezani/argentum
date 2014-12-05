1c97ef15ca2bf9a332e6acfee5c8765c886ee383
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2014-12-04 22:57:31 -0200

    Initial Commit

---
+++ src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
@@ -0,0 +1,33 @@
+package br.com.caelum.argentum.modelo;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class CandlestickFactory {
+	
+	public Candlestick constroiCandleParaData(Calendar data,
+			List<Negociacao> negociacoes) {
+
+		double maximo = 0;
+		double minimo = Double.MAX_VALUE;
+		double volume = 0;
+
+		// digite foreach e de um ctrl + espaço para ajudar a criar o bloco	abaixo!
+		for (Negociacao negociacao : negociacoes) {
+			volume += negociacao.getVolume();
+			if (negociacao.getPreco() > maximo) {
+				maximo = negociacao.getPreco();
+			} else if (negociacao.getPreco() < minimo) {
+				minimo = negociacao.getPreco();
+			}
+		}
+		
+		double abertura = negociacoes.isEmpty() ? 0 : negociacoes.get(0).getPreco();
+		double fechamento = negociacoes.isEmpty() ? 0 : negociacoes.get(negociacoes.size() - 1).getPreco();
+		minimo = negociacoes.size() == 1 ? negociacoes.get(0).getPreco() : minimo;
+		
+		return new Candlestick(abertura, fechamento, minimo, maximo, volume,
+				data);
+	}
+	
+}
---
+++ src/test/java/br/com/caelum/argentum/modelo/NegociacaoTest.java
@@ -0,0 +1,23 @@
+package br.com.caelum.argentum.modelo;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+public class NegociacaoTest {
+
+	@Test
+	public void dataDaNegociacaoEhImutavel() {
+	// se criar um negocio no dia 15...
+	Calendar c = Calendar.getInstance();
+	c.set(Calendar.DAY_OF_MONTH, 15);
+	Negociacao negociacao = new Negociacao(10, 5, c);
+	// ainda que eu tente mudar a data para 20...
+	negociacao.getData().set(Calendar.DAY_OF_MONTH, 20);
+	// ele continua no dia 15.
+	assertEquals(15, negociacao.getData().get(Calendar.DAY_OF_MONTH));
+	}
+
+}
